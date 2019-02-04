575fe3a54fe6f545e7ffa51bc8149fd29f51a301
Kate Lawrence-Gupta <39601928+klawrencegupta-splunk@users.noreply.github.com>
2019-02-04 17:04:43 -0500

    fix in line 15
    needs relative directly for proper tar

--- splunk_strace.sh
+++ splunk_strace.sh
@@ -1,16 +1,15 @@
-
 #!/bin/bash
- 
+
 #strace shell script - strace.sh
 # $1 is the directory for the logs
 # $2 is the timeout for the run of the strace
- 
+
 for x in $(ps aux | grep splunkd | awk '{print $2}')
 do timeout $2 strace -fo $1'/strace'_$x.summary.txt -c -p $x
 done
- 
+
 for x in $(ps aux | grep splunkd | awk '{print $2}')
 do timeout $2 strace -fo $1'/strace'_$x.full.txt -p $x
 done
- 
-tar -cf strace_outputs.tar *strace*.txt; rm -f *strace*.txt
+
+tar -cf $1/strace_outputs.tar $1/*strace*.txt; rm -f $1/*strace*.txt
